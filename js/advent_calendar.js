f4610317313817bcaa52ed35412bf1daa391fc4f
yurg <yurg@users.noreply.github.com>
2021-11-10 10:23:34 +0100

    Use Drupal JS, reduce a number of $.ajax calls

--- js/advent_calendar.js
+++ js/advent_calendar.js
@@ -1,39 +1,45 @@
-$(function () {
+(function ($, Drupal) {
+  'use strict';
+  Drupal.behaviors.advent_calendar = {
+    attach: function (context, settings) {
 
+  
+/* Do not edit above */
+/************************/
   var dateTime = new Date();
   var day = dateTime.getDate();
-  $("div.square").each(function () {
+      // See https://drupal.stackexchange.com/questions/292008/why-does-this-simple-javascript-execute-multiple-times for details of below row changes
+  $('.da-thumbs li', context).once('advent_calendar').each(function () {
     // date check for links
-    var date = parseInt($(this).attr('data-date'));
-    var path = "";
-    if (date <= day) {
-      path = $(this).attr('id');
-      $(this).wrap($('<a>', {
-        href: "node/" + path
-      }));
-
+    var date = parseInt($(this).text());
+    //var path = "";
+  var path = $(this).attr('class').split("-")[1];
+if (date <= day) {
 // setting up onclick boolean to local storage for eligible days
       $(this).click(function () {
-        localStorage.setItem($(this).attr('id'), "1");
+        localStorage.setItem($(this).attr('class'), "1");
       });
+    }
 // change the images based on the local storage boolean
-      if (localStorage.getItem($(this).attr('id')) === "1") {
-        var imgUrl = "";
+    if (localStorage.getItem($(this).attr('class')) === "1" && path.length > 0 ) {
+        var imgUrl = '';
+        var niceUrl = '';
         $.ajax({
-          url: "api/content/" + path + "?_format=json",
+          url: "/data/content/" + path + "?_format=json",
           async: false,
           dataType: 'json',
           success: function (json_data) {
-            var html = $.parseHTML( json_data[0].body[0].value );
-            var source = html[0].childNodes[0].src;
-            imgUrl = source;
+            imgUrl = json_data[0].field_image;
+            niceUrl = json_data[0].view_node;
+                    
           }
         });
-        $(this).find('img').attr("src", imgUrl);
+        $(this).html('<a href="' + niceUrl + '"><h2>' + date + '</h2>' + '<img src='+  imgUrl +'></a>');
       }
-    }
-  });
-});
+   });
+  }
+ };
+})(jQuery, Drupal,drupalSettings);
 
 
 
